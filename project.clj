dba2ef5cb608793e276332de49d15431113581a4
guns <self@sungpae.com>
2014-04-27 04:24:50 -0500

    Remove 1.2 and 1.3 deps
    test.check is only compatible with Clojure 1.4.0+.
    
    This library probably works just fine with all released versions of
    Clojure.

--- project.clj
+++ project.clj
@@ -5,9 +5,7 @@
             :url "https://www.ruby-lang.org/en/about/license.txt"}
   :dependencies []
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.5.7"]]}
-             :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
-  :aliases {"test-all" ["with-profile" "dev,1.2:dev,1.3:dev,1.4:dev,1.5:dev,1.6" "test"]})
+  :aliases {"test-all" ["with-profile" "dev,1.4:dev,1.5:dev,1.6" "test"]})
